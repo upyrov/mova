876f4407437d71a3fc8086fdacdff0352efe05e7
upyrov <upyrov@outlook.com>
2025-11-22 15:29:32 +0200

    feat: add borrow checker

--- src/interpreter/evaluation.rs
+++ src/interpreter/evaluation.rs
@@ -2,10 +2,7 @@ use std::{cell::RefCell, rc::Rc};
 
 use crate::{
     error::{MovaError, Result},
-    interpreter::{
-        data::{Data, Reference},
-        scope::{Scope, borrow_scope},
-    },
+    interpreter::{data::Data, scope::Scope},
     parser::{expression::Expression, node::Node, statement::Statement},
 };
 
@@ -22,7 +19,7 @@ fn evaluate_binary_expression(operator: &str, left: Data, right: Data) -> Result
             }
         }
         (o, l, r) => Err(MovaError::Runtime(format!(
-            "Unexpected operator `{o}` for operands: {l:?} and {r:?}",
+            "Unexpected operator '{o}' for operands '{l:?}' and '{r:?}'",
         ))),
     }
 }
@@ -33,16 +30,13 @@ fn evaluate_call(
     arguments: Rc<[Expression]>,
 ) -> Result<Option<Data>> {
     // Drop immediately after use so that recursive calls don't panic
-    let function_data = {
-        let mut s = borrow_scope(&scope)?;
-        s.resolve(name)?
-    };
+    let function_data = { scope.borrow_mut().resolve(name)? };
 
     match function_data {
         Data::Function {
             parameters,
             body,
-            body_scope,
+            definition_scope,
         } => {
             let argument_count = arguments.len();
             let parameter_count = parameters.len();
@@ -53,58 +47,33 @@ fn evaluate_call(
                 )));
             }
 
-            let evaluated_arguments = arguments
-                .into_iter()
+            let evaluated_arguments: Vec<Data> = arguments
+                .iter()
                 .map(|argument| {
                     let node = Rc::new(Node::Expression(Rc::new(argument.clone())));
                     let data = evaluate(node, Rc::clone(&scope))?.ok_or(MovaError::Runtime(
                         "Expected expression, but received statement as argument".into(),
                     ))?;
-                    match data {
-                        Data::Reference(r) => {
-                            let mut s = borrow_scope(&scope)?;
-                            let found_scope =
-                                s.find_scope_by_identifier(Rc::clone(&scope), &r.identifier)?;
-                            let referenced_value = s.resolve(&r.identifier)?;
-
-                            let value = Rc::new(referenced_value.clone());
-                            let reference = Reference {
-                                identifier: Rc::clone(&r.identifier),
-                                scope: found_scope,
-                                value: Rc::clone(&value),
-                            };
-                            borrow_scope(&body_scope)?.borrow(reference);
-
-                            Ok(referenced_value)
-                        }
-                        _ => Ok(data),
-                    }
+                    Ok(data)
                 })
-                .collect::<Vec<_>>();
+                .collect::<Result<Vec<Data>>>()?;
 
+            // Avoid interfering with other calls
+            let execution_scope =
+                Rc::new(RefCell::new(Scope::new(Some(Rc::clone(&definition_scope)))));
             {
-                let mut s = borrow_scope(&body_scope)?;
+                let mut s = execution_scope.borrow_mut();
 
                 // Map arguments to parameters
                 evaluated_arguments
                     .into_iter()
                     .zip(parameters.iter())
-                    .try_for_each(|(data, parameter)| {
-                        s.declare(parameter, data?);
-                        Ok(())
-                    })?;
+                    .for_each(|(data, parameter)| s.declare(parameter, data));
             }
 
-            let result = evaluate(
-                Rc::new(Node::Expression(Rc::clone(&body))),
-                Rc::clone(&body_scope),
-            );
-            borrow_scope(&body_scope)?.return_references()?;
-            result
+            evaluate(Rc::new(Node::Expression(Rc::clone(&body))), execution_scope)
         }
-        _ => Err(MovaError::Runtime(format!(
-            "Identifier {name} is not callable",
-        ))),
+        _ => Err(MovaError::Runtime(format!("'{name}' is not callable",))),
     }
 }
 
@@ -113,23 +82,15 @@ fn evaluate_expression(
     scope: Rc<RefCell<Scope>>,
 ) -> Result<Option<Data>> {
     match &*expression {
-        Expression::Identifier(i) => Ok(Some(borrow_scope(&scope)?.resolve(&i)?)),
         Expression::Number(n) => Ok(Some(Data::Number(*n))),
         Expression::Boolean(b) => Ok(Some(Data::Boolean(*b))),
+        Expression::Identifier(i) => Ok(Some(scope.borrow_mut().resolve(i)?)),
         Expression::Reference(r) => {
-            let mut s = borrow_scope(&scope)?;
-            let found_scope = s.find_scope_by_identifier(Rc::clone(&scope), &r)?;
-            let referenced_value = s.resolve(&r)?;
-
-            let value = Rc::new(referenced_value.clone());
-            let reference = Reference {
-                identifier: Rc::clone(&r),
-                scope: found_scope,
-                value: Rc::clone(&value),
-            };
-            s.borrow(reference);
-
-            Ok(Some(referenced_value))
+            let reference = scope.borrow_mut().borrow(r)?;
+            match reference {
+                Data::Reference(r) => Ok(Some(r.value())),
+                _ => unreachable!(),
+            }
         }
         Expression::BinaryExpression {
             operator,
@@ -143,6 +104,7 @@ fn evaluate_expression(
             .ok_or(MovaError::Runtime(
                 "Expected expression, but received statement as left operand".into(),
             ))?;
+
             let right = evaluate(
                 Rc::new(Node::Expression(Rc::clone(right))),
                 Rc::clone(&scope),
@@ -150,24 +112,26 @@ fn evaluate_expression(
             .ok_or(MovaError::Runtime(
                 "Expected expression, but received statement as right operand".into(),
             ))?;
+
             Ok(Some(evaluate_binary_expression(&operator, left, right)?))
         }
         Expression::Call { name, arguments } => evaluate_call(scope, &name, Rc::clone(arguments)),
         Expression::Block(b) => {
             let child_scope = Rc::new(RefCell::new(Scope::new(Some(Rc::clone(&scope)))));
-            let mut results: Vec<_> = b
-                .into_iter()
-                .map(|n| evaluate(Rc::new(n.clone()), Rc::clone(&child_scope)))
-                .collect();
-            borrow_scope(&child_scope)?.return_references()?;
-            Ok(results.pop().transpose()?.flatten())
+            let mut result = None;
+            for node in b.into_iter() {
+                drop(result);
+                result = evaluate(Rc::new(node.clone()), Rc::clone(&child_scope))?;
+            }
+            Ok(result)
         }
         Expression::Program(p) => {
-            let mut results: Vec<_> = p
-                .into_iter()
-                .map(|n| evaluate(Rc::new(n.clone()), Rc::clone(&scope)))
-                .collect();
-            Ok(results.pop().transpose()?.flatten())
+            let mut result = None;
+            for node in p.into_iter() {
+                drop(result);
+                result = evaluate(Rc::new(node.clone()), Rc::clone(&scope))?;
+            }
+            Ok(result)
         }
     }
 }
@@ -180,22 +144,22 @@ fn evaluate_statement(statement: Rc<Statement>, scope: Rc<RefCell<Scope>>) -> Re
                 Rc::clone(&scope),
             )?
             .ok_or(MovaError::Runtime(
-                "Expected expression, but received statement".into(),
+                "Expected expression, but received statement as value".into(),
             ))?;
-            borrow_scope(&scope)?.declare(&name, data);
+            scope.borrow_mut().declare(&name, data);
         }
         Statement::Function {
             name,
             parameters,
             body,
-        } => borrow_scope(&scope)?.declare(
-            &name,
-            Data::Function {
+        } => {
+            let function = Data::Function {
                 parameters: Rc::clone(parameters),
                 body: Rc::clone(body),
-                body_scope: Rc::new(RefCell::new(Scope::new(Some(Rc::clone(&scope))))),
-            },
-        ),
+                definition_scope: Rc::new(RefCell::new(Scope::new(Some(Rc::clone(&scope))))),
+            };
+            scope.borrow_mut().declare(&name, function);
+        }
     }
     Ok(())
 }
